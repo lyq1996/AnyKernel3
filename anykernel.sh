0f1eda0db2f62c7516d2c6b1191da2d74d62289f
lyq1996 <lyq123233@gmail.com>
2020-03-26 15:43:08 +0800

    update for my kernel

--- anykernel.sh
+++ anykernel.sh
@@ -4,23 +4,22 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=Oneplus Offical kernel with Clang @ xda-developers
 do.devicecheck=1
 do.modules=0
-do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
+device.name1=dumpling
+device.name2=cheeseburger
+device.name3=
+device.name4=
 device.name5=
-supported.versions=
+supported.versions=9.0.0
 supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 
