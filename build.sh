c0abfc99d339b4f1c3b1875e8c1e2196cd9fac5d
lyq1996 <lyq123233@gmail.com>
2020-04-05 11:23:56 +0800

    use relative path

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 # !/bin/sh
 #cp /root/android_kernel_oneplus_msm8998/out/arch/arm64/boot/Image ./
 #cp /root/android_kernel_oneplus_msm8998/out/arch/arm64/boot/Image.gz ./
-cp /root/android_kernel_oneplus_msm8998/out/arch/arm64/boot/Image.gz-dtb ./
+cp ../android_kernel_oneplus_msm8998/out/arch/arm64/boot/Image.gz-dtb ./
 
 zip -r9 OP5_5T-lcblues-Kernel-EAS-9.02.zip * -x .git README.md *placeholder build.sh build1.sh *.zip
 
--- build1.sh
+++ build1.sh
@@ -1,8 +1,8 @@
 # !/bin/sh
 #cp /root/android_kernel_oneplus_msm8998/out/arch/arm64/boot/Image ./
 #cp /root/android_kernel_oneplus_msm8998/out/arch/arm64/boot/Image.gz ./
-cp /root/android_kernel_oneplus_msm8998/out/arch/arm64/boot/Image.gz-dtb ./
+cp ../android_kernel_oneplus_msm8998/out/arch/arm64/boot/Image.gz-dtb ./
 
-zip -r9 OP5_5T-lcblues-Kernel-EAS-9.02-PE.zip * -x .git README.md *placeholder build.sh build1.sh *init.lcblues_boot.sh *init.qcom.rc *.zip
+zip -r9 other_kernel.zip * -x .git README.md *placeholder build.sh build1.sh *init.lcblues_boot.sh *init.qcom.rc *.zip
 
 
