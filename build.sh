eb454fc49b840be3c8fff662398c6606773475b9
lyq1996 <lyq123233@gmail.com>
2020-04-05 14:31:56 +0800

    update for my kernel

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ cp ../android_kernel_oneplus_msm8998/out/arch/arm64/boot/Image.gz-dtb ./
 
 zip -r9 OP5_5T-lcblues-Kernel-EAS-9.03+.zip * -x .git README.md *placeholder build.sh build1.sh *.zip
 
-
+rm Image.gz-dtb
--- build1.sh
+++
@@ -1,8 +0,0 @@
-# !/bin/sh
-#cp /root/android_kernel_oneplus_msm8998/out/arch/arm64/boot/Image ./
-#cp /root/android_kernel_oneplus_msm8998/out/arch/arm64/boot/Image.gz ./
-cp ../android_kernel_oneplus_msm8998/out/arch/arm64/boot/Image.gz-dtb ./
-
-zip -r9 other_kernel.zip * -x .git README.md *placeholder build.sh build1.sh *init.lcblues_boot.sh *init.qcom.rc *.zip
-
-
